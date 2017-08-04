5da19662fcf5efe98a71394975d0295d3d62fe68
etraineva <elenaot.et@gmail.com>
2017-08-04 15:14:27 +0300

    Lambda examples start

---
+++ src/Lambda.java
@@ -0,0 +1,18 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Lambda examples
+ * Created by developer on 04.08.17.
+ */
+public class Lambda {
+
+    private static final Logger LOGGER = Logger.getLogger( Lambda.class.getName() );
+
+    public void example_1(){
+
+        LOGGER.log( Level.INFO, "Programm strart!" );
+
+    }
+
+}
---
+++ test/LambdaTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Test lambda examples
+ * Created by developer on 04.08.17.
+ */
+class LambdaTest {
+    @Test
+    void example_1() {
+
+        Lambda lambda = new Lambda();
+        lambda.example_1();
+
+    }
+
+}
