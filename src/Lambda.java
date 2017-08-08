e7ca4a92c3de2b9585b73c78120bae195f80cc18
etraineva <elenaot.et@gmail.com>
2017-08-08 16:34:07 +0300

    Add sum functions with lambda

--- src/Lambda.java
+++ src/Lambda.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,6 +16,74 @@ public class Lambda {
 
         LOGGER.log( Level.INFO, "Programm strart!" );
 
+        System.out.println();
+        System.out.println("Standart print: ");
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
+        System.out.println();
+        System.out.println("Lambda print: ");
+
+        numbers.forEach((Integer value) -> System.out.print(value + " "));
+
+        System.out.println();
+        System.out.println("Lambda print: ");
+
+        numbers.forEach(value -> System.out.print(value + " "));
+
+        System.out.println();
+        System.out.println("Lambda print: ");
+
+        numbers.forEach(System.out::print);
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Sum of all elements:");
+        System.out.println("Standart print: ");
+        System.out.println(sumAll(numbers));
+        System.out.println("Lambda print: ");
+        System.out.println(sumAll(numbers, n -> true));
+        System.out.println("Sum of n%2==0 elements:");
+        System.out.println("Standart print: ");
+        System.out.println(sumAllEven(numbers));
+        System.out.println("Lambda print: ");
+        System.out.println(sumAll(numbers, n -> n % 2 == 0));
+        System.out.println("Sum of n->n>3 elements:");
+        System.out.println(sumAll(numbers, n -> n > 3));
+
+    }
+
+    public int sumAll(List<Integer> numbers) {
+        int total = 0;
+        for (int number : numbers) {
+            total += number;
+        }
+        return total;
+    }
+
+    public int sumAllEven(List<Integer> numbers) {
+        int total = 0;
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                total += number;
+            }
+        }
+        return total;
+    }
+
+
+
+    public int sumAll(List<Integer> numbers, Predicate<Integer> p) {
+        int total = 0;
+        for (int number : numbers) {
+            if (p.test(number)) {
+                total += number;
+            }
+        }
+        return total;
     }
 
 }
